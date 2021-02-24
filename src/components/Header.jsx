9c67e4708260d312b6b36f05d4cb438215dc6c1d
Karol Wiśniewski <karolwisniewski92@gmail.com>
2021-02-24 21:31:23 +0100

    utworzono store i przypięto odczyt do Header. Dodano też warunkowe wyświetlanie przycisków zaloguj / wyloguj

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,75 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { firebaseApp } from '../fbase';
+import { connect } from 'react-redux';
 
-class Header extends React.Component {
+class HeaderComponent extends React.Component {
+
+    checkLogged = () => {
+        let isLogin;
+        let user = firebaseApp.auth().currentUser;
+        // console.log(user);
+        if (user === null) {
+            isLogin = false
+        } else {
+            isLogin = true
+        }
+        return isLogin;
+    }
+
+    logOut = () => {
+        firebaseApp.auth().signOut()
+            .then(() => {
+                console.log(`Wylogowano poprawnie`);
+                let user = firebaseApp.auth().currentUser;
+                console.log(user);
+
+            }).catch((error) => {
+                console.log(error);
+            });
+    }
+
+
+
     render() {
-        return (<div className="wd-100 h100 bcg-lightgray p-10 flex-row">
-            <div className="wd-25 bcg-yellow">Logo</div>
-            <div className="wd-50 bcg-yellow mlr-10">Menu Content</div>
-            <div className="wd-25 bcg-yellow flex-center">
-                <Link to="/logIn"><button className="btn btn-blue">Zaloguj</button></Link>
-                <Link to="/createAccount"><button className="btn btn-blue">Zarejestruj</button></Link>
-            </div>
-        </div>)
+
+        // this.checkLogged()
+        // console.log(this.checkLogged());
+        return (
+            <div className="wd-100 h100 bcg-lightgray p-10 flex-row" >
+                <div className="wd-25 bcg-yellow">Logo</div>
+                <div className="wd-50 bcg-yellow mlr-10">Menu Content</div>
+                <div className="wd-25 bcg-yellow flex-center">
+                    {console.log(this.props.logIn)}
+
+                    {!this.props.logIn &&
+                        <React.Fragment>
+                            <Link to="/logIn"><button className="btn btn-blue">Zaloguj</button></Link>
+                            <Link to="/createAccount"><button className="btn btn-blue">Zarejestruj</button></Link>
+                        </React.Fragment>
+
+                    }
+
+                    {this.props.logIn &&
+                        <React.Fragment>
+                            <button className="btn btn-red" onClick={this.logOut}>Wyloguj</button>
+                        </React.Fragment>
+
+                    }
+
+
+
+                </div>
+            </div>)
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        logIn: state.logIn
+    }
+
+}
+
+const Header = connect(mapStateToProps)(HeaderComponent)
 export default Header;
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from './App';
 import Dashboard from './Dashboard';
-import RegisterForm from './RegisterForm'
-import LogInForm from './LogInForm'
+import RegisterForm from './RegisterForm';
+import LogInForm from './LogInForm';
+import { Provider } from 'react-redux';
+import store from '../store/store';
 
 export default class Router extends React.Component {
 
     render() {
         return (
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" component={App} />
-                    <Route exact path="/dashboard" component={Dashboard} />
-                    <Route exact path="/logIn" component={LogInForm} />
-                    <Route exact path="/createAccount" component={RegisterForm} />
-                    {/* <Route component={PageNotFound} /> */}
-                </Switch>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component={App} />
+                        <Route exact path="/dashboard" component={Dashboard} />
+                        <Route exact path="/logIn" component={LogInForm} />
+                        <Route exact path="/createAccount" component={RegisterForm} />
+                        {/* <Route component={PageNotFound} /> */}
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
         )
     }
 }
